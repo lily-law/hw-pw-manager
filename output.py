ea44953d74e8abcad8067208c89ef2cc14c00437
Lily Law <lilylaw.dev@gmail.com>
2021-11-03 12:30:31 +0000

    Update serial port for Raspi A+ V1.1

--- output.py
+++ output.py
@@ -1,15 +1,15 @@
 import serial
 
-def transmit(txt):
+def send(txt):
     error = False
     with serial.Serial() as ser:
         ser.baudrate = 19200
-        ser.port = 'COM1' # '/dev/ttyS0', # ttyAM0 on < PI 3   
+        ser.port = '/dev/ttyAMA0' # '/dev/ttyS0', # ttyAMA0 on < PI 3
     try:
         ser.open()
         ser.write(bytes(txt, 'utf-8'))
     except:
-        error = 'Unable to transmit'
+        error = 'Unable to send. Is serial port correct? Is serial is enabled in raspi-config? Try disabling and re enabling it.'
     finally:
         if ser.is_open:
             ser.close()
